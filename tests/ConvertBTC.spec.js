ce23d38e52bfea5bced1a49abfc4cfe21b5090db
Virginia Cruz <virginiarcruz@gmail.com>
2018-03-20 08:28:37 -0300

    concluindo a biblioteca do bitcoint converter, teste de moedas, cores e loader

---
+++ tests/ConvertBTC.spec.js
@@ -0,0 +1,86 @@
+const nock = require('nock');
+const chalk = require('chalk');
+const chai = require('chai');
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
+const expect = chai.expect;
+
+chai.use(sinonChai);
+
+const convertBTC = require('../src/ConvertBTC');
+
+describe('ConvertBTC', () => {
+
+  let consoleStub;
+
+  const responseMock = {
+    "success": true,
+    "time": "2018-03-20 10:22:58",
+    "price": 8465.93
+  };
+
+  beforeEach(() =>{
+    consoleStub = sinon.stub(console, 'log');
+  });
+
+  afterEach( () => {
+    console.log.restore();
+  });
+
+  it('should ruse currency USD and 1 amount default', (done) => {
+    //https://apiv2.bitcoinaverage.com/convert/global?from=BTC&to=USD&amount=1
+    nock('https://apiv2.bitcoinaverage.com')
+      .get('/convert/global')
+      .query({ from: 'BTC', to: 'USD', amount: 1 })
+      .reply(200, responseMock);
+
+    convertBTC();
+
+    setTimeout(() => {
+      expect(consoleStub).to.have.been.calledWith(`${chalk.red(1)} BTC to ${chalk.cyan('USD')} = ${chalk.yellow(8465.93)}`);
+      done();
+    }, 300)
+  });
+
+  it('should ruse currency USD and 10 as amount', (done) => {
+    nock('https://apiv2.bitcoinaverage.com')
+      .get('/convert/global')
+      .query({ from: 'BTC', to: 'USD', amount: 10 })
+      .reply(200, responseMock);
+
+    convertBTC('USD', 10);
+
+    setTimeout(() => {
+      expect(consoleStub).to.have.been.calledWith(`${chalk.red(10)} BTC to ${chalk.cyan('USD')} = ${chalk.yellow(8465.93)}`);
+      done();
+    }, 300)
+  });
+
+  it('should ruse currency BRL and 1 as amount default', (done) => {
+    nock('https://apiv2.bitcoinaverage.com')
+      .get('/convert/global')
+      .query({ from: 'BTC', to: 'BRL', amount: 1 })
+      .reply(200, responseMock);
+
+    convertBTC('BRL');
+
+    setTimeout(() => {
+      expect(consoleStub).to.have.been.calledWith(`${chalk.red(1)} BTC to ${chalk.cyan('BRL')} = ${chalk.yellow(8465.93)}`);
+      done();
+    }, 300)
+  });
+
+  it('should message user when api reply with error', (done) => {
+    nock('https://apiv2.bitcoinaverage.com')
+      .get('/convert/global')
+      .query({ from: 'BTC', to: 'BRL', amount: 1 })
+      .replyWithError('Error');
+
+    convertBTC('BRL');
+
+    setTimeout(() => {
+      expect(consoleStub).to.have.been.calledWith(chalk.red('Something went wrong'));
+      done();
+    }, 300)
+  });
+});
--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,5 +1,39 @@
-import { expect } from 'chai';
+const expect = require('chai').expect;
 
-describe('Main', () => {
+const exec = require('child_process').exec;
+const btcConverter = './src/main.js';
+const pkg = require('../package.json');
 
+describe('Main CLI', () => {
+  it('should return version of btc-converter', (done) => {
+    exec(`${btcConverter} --version`, (err, stdout, stderr) => {
+      if(err) throw err;
+      expect(stdout.replace('\n', '')).to.be.equal(pkg.version);
+      done();
+    });
+  });
+
+  it('should return description when btc-converter --help', (done) => {
+    exec(`${btcConverter} --help`, (err, stdout, stderr) => {
+      if(err) throw err;
+      expect(stdout.includes('Convert Bitcoin to any currency defined')).to.be.true;
+      done();
+    });
+  });
+
+  it('should return currency option when btc-converter --help', (done) => {
+    exec(`${btcConverter} --help`, (err, stdout, stderr) => {
+      if(err) throw err;
+      expect(stdout.includes('--currency')).to.be.true;
+      done();
+    });
+  });
+
+  it('should return amount option when btc-converter --help', (done) => {
+    exec(`${btcConverter} --help`, (err, stdout, stderr) => {
+      if(err) throw err;
+      expect(stdout.includes('--amount')).to.be.true;
+      done();
+    });
+  });
 });
